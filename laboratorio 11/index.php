c378ac7cb237ddc6181e3da53ed358128651d0b1
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-08 18:50:01 +0100

    idk what i am doing

---
+++ laboratorio 11/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>Homepage</title>
+        <link rel="stylesheet" type="text/css" href="./style.css">
+
+    </head>
+
+    <body>
+        <header>
+            <img class="web_site_logo" src="logo.jpg" alt="logo" width="150" height="150">
+
+
+            <nav class="navbar">
+                <ul>
+                    <li><a href="./index.php">Home</a></li>
+                    <li><a href="news.asp">News</a></li>
+                    <li><a href="contact.asp">Contact</a></li>
+                    <li><a href="about.asp">About</a></li>
+                </ul>
+            </nav>
+
+        </header>
+
+
+
+
+
+    </body>
+</html>
