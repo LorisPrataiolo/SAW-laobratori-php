e06a6dce5016e6f6a7e634c76ec0634b06f021a4
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-12 20:20:56 +0100

    Fixed Style.css

--- laboratorio 11/SignIn.php
+++ laboratorio 11/SignIn.php
@@ -8,16 +8,28 @@
 </head>
 <body>
 
-<header>
-    <!-- Contenuto dell'header -->
-</header>
+    <header>
+        <img class="web_site_logo" src="logo.jpg" alt="logo" width="150" height="150">
+
+        <h3>Insert E-mail & password and Click SignIn</h3>
+        <nav class="navbar">
+            
+                <a href="./index.php">Home</a>
+                <a href="./SignIn.php">Sing-In</a>
+                <a href="contact.asp">Contact</a>
+                <a href="about.asp">About</a>
+            
+        </nav>
+        
+
+
+    </header>
 
 <main>
     <!-- Contenuto principale della pagina -->
-    <h3>Insert E-mail & password and Click SignIn</h3>
     <form method="post" action="./logInVerfy.php">
         <label for="email">Email</label><br>
-        <input type="email" id="email" name="email">
+        <input type="email" id="email" name="email"><br>
 
         <label for="password">Password</label><br>
         <input type="password" id="password" name="password"><br>
