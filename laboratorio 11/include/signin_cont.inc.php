66d5645c47fa9d950ec94293f31e531df5a3a756
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-14 16:59:52 +0100

    is_email_talen

--- laboratorio 11/include/signin_cont.inc.php
+++ laboratorio 11/include/signin_cont.inc.php
@@ -14,8 +14,13 @@
         return filter_var($email, FILTER_VALIDATE_EMAIL) != FALSE ?  TRUE :  FALSE;
     }
 
-    function is_email_taken($email) {
-        return filter_var($email, FILTER_VALIDATE_EMAIL) != FALSE ?  TRUE :  FALSE;
+
+
+    /* la seguente funzione restituisce TRUE se l'email  e' gia' presente all'interno del
+    database, FALSE altrimenti */
+    function is_email_taken(object $pdo, string $email) {
+        
+        return (bool) get_email($pdo, $email);
     }
 
 
--- laboratorio 11/include/signin_model.inc.php
+++ laboratorio 11/include/signin_model.inc.php
@@ -2,4 +2,27 @@
 
     declare (strict_types = 1);
 
+
+    /*  */
+    function get_email(object $pdo, string $email) {
+
+   
+        $query = "SELECT email FROM users WHERE email = :email;";
+
+    // Preparazione della query
+        $stmt = $pdo->prepare($query);
+
+    // Associazione del parametro :email con il valore della variabile $email
+        $stmt->bindParam(":email", $email);
+
+    // Esecuzione della query
+        $stmt->execute();
+
+    // Recupero della riga risultante come un array associativo
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result;
+
+    }
+
 ?>
