353f8f2dda2468136fdb16b470521f3ca7276038
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-14 13:58:54 +0100

    restyle

--- laboratorio 11/pages/login.php
+++ laboratorio 11/pages/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign-In</title>
+    <title>Log In</title>
     <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
@@ -11,10 +11,10 @@
     <header>
         <img class="web_site_logo" src="../img/logo.jpg" alt="logo" width="150" height="150">
 
-        <h3>Join to Project Unicorn</h3>
+        <h3>LogIn</h3>
         <nav class="navbar">
             
-                <a href="../index.php">Home</a>
+                <a href="/">Home</a>
                 <a href="./signin.php">Sing-In</a>
                 <a href="contact.asp">Contact</a>
                 <a href="about.asp">About</a>
@@ -35,10 +35,7 @@
         <input type="password" id="password" name="password"><br>
 
 
-        <label for="repeatpassword">Repeat your Password</label><br>
-        <input type="password" id="repeatpassword" name="repeatpassword"><br>
-
-        <input type="submit" name="Sign-In">
+        <input type="submit" name="Sign In">
     </form>
 </main>
 
